33d4162b84e62ad14b760964f479cf70af429a29
Roberto Chasipanta <maniac787@gmail.com>
2022-10-26 18:27:10 -0500

    JWT register

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,11 @@ const express = require("express");
 const {validatorRegister, validatorLogin} = require("../validators/auth");
 const router = express.Router();
 
-const {loginController} = require("../controllers/auth");
+const {registerController} = require("../controllers/auth");
 
 //TODO localhost:3000/api/auth/login
 //TODO localhost:3000/api/auth/register
-router.post("/register", validatorRegister, loginController);
+router.post("/register", validatorRegister, registerController);
 
 router.post("/login", validatorLogin, (res, req) => {
 
